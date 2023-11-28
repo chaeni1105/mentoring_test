730d70e10763a513dc453ecc9c440cddfaf0fc7c
Chaeeun Lee <cyleecodms@naver.com>
2023-11-28 20:57:55 +0900

    feat: 채은 주석 추가

--- test.py
+++ test.py
@@ -1,2 +1,7 @@
 # 저는 이채은입니다.
 # 드디어 모두가 연결이 되었습니다.
+
+# git pull origin                       -> pull
+# git add .                             -> add
+# git commit -m "comment: OOO주석 추가"  -> commit
+# git push origin                       -> push
